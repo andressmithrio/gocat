5df929c64a6b9189c5b7411882558e2ec6f9ab68
Andres Smith <andres.smith@reflek.io>
2024-11-11 20:49:18 +0100

    Add support for IndexListExpr

--- main.go
+++ main.go
@@ -495,6 +495,11 @@ func walkBeforeAfter(x interface{}, before, after func(interface{})) {
 	case *ast.IndexExpr:
 		walkBeforeAfter(&n.X, before, after)
 		walkBeforeAfter(&n.Index, before, after)
+	case *ast.IndexListExpr:
+		walkBeforeAfter(&n.X, before, after)
+		for _, index := range n.Indices {
+			walkBeforeAfter(index, before, after)
+		}
 	case *ast.SliceExpr:
 		walkBeforeAfter(&n.X, before, after)
 		if n.Low != nil {
